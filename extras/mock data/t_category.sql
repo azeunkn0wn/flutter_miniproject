78512d3b34b9b0f4e888406d7e7cd5c2398f0833
azeunkn0wn <azeunkn0wn@gmail.com>
2020-05-09 10:30:08 +0800

    .gitignore fix

--- extras/mock data/t_category.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO t_category (category_id, category_name) VALUES ('1', 'music');
-INSERT INTO t_category (category_id, category_name) VALUES ('2', 'math');
-INSERT INTO t_category (category_id, category_name) VALUES ('3', 'programming');
-
